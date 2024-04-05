9fec5c3bb51f037e3c7851a40440e4a32a18cdc7
Gui027 <guilhermerg1234@gmail.com>
2024-04-05 09:34:09 -0300

    Criando algumas rotas

--- 01 - Cadastro/index.js
+++ 01 - Cadastro/index.js
@@ -25,8 +25,10 @@ app.listen(3000);
 console.log("servidor rodando em: localhost: 3000");
 
 app.post("/insert", urlencodesParser, function (req, res) {
+  //Através desse post o usuário será capaz de inserir dados ao nosso cadastro.
   let objJSON = {};
   if (req.body.code_user) objJSON.code_user = req.body.code_user;
+  // Verifica se o usuário contratou o serviço de chatbot
   else objJSON.code_user = 0;
   if (req.body.code_session) objJSON.code_session = req.body.code_session;
   else objJSON.code_session = 0;
@@ -43,3 +45,115 @@ app.post("/insert", urlencodesParser, function (req, res) {
     res.send(result);
   });
 });
+
+function cod() {
+  const data = new Date();
+  const ano = data.getFullYear();
+  const mes = data.getMonth();
+  const dia = data.getDate();
+  const hora = data.getHours();
+  const minuto = data.getMinutes();
+  const segundo = data.getSeconds();
+  const milesegundos = data.getMilliseconds();
+  const result =
+    Number(
+      ano +
+        "" +
+        mes +
+        "" +
+        dia +
+        "" +
+        hora +
+        "" +
+        minuto +
+        "" +
+        segundo +
+        "" +
+        milesegundos
+    ) / 2;
+  return result;
+}
+
+app.post("/update", urlencodesParser, function (req, res) {
+  let objJSON = {};
+  if (req.body.code_user) objJSON.code_user = req.body.code_user;
+  else objJSON.code_user = 0;
+  if (req.body.code_session) objJSON.code_session = req.body.code_session;
+  if (req.body.code_current) objJSON.code_current = req.body.code_current;
+  else objJSON.code_current = 0;
+  if (req.body.code_before) objJSON.code_before = req.body.code_before;
+  if (req.body.input) objJSON.input = req.body.input;
+  if (req.body.output) objJSON.output = req.body.output;
+
+  updateData(objJSON, function (result) {
+    res.send(result);
+  });
+});
+
+app.post("/delete", urlencodesParser, function (req, res) {
+  let objJSON = {};
+  if (req.body.code_user) objJSON.code_user = req.body.code_user;
+  else objJSON.code_user = 0;
+  if (req.body.code_session) objJSON.code_session = req.body.code_session;
+  if (req.body.code_current) objJSON.code_current = req.body.code_current;
+  else objJSON.code_current = 0;
+  if (req.body.code_before) objJSON.code_before = req.body.code_before;
+  if (req.body.input) objJSON.input = req.body.input;
+  if (req.body.output) objJSON.output = req.body.output;
+
+  deleteData(objJSON, function (result) {
+    res.send(result);
+  });
+});
+
+app.post("/find", urlencodesParser, function (req, res) {
+  let objJSON = {};
+  if (req.body.code_user) objJSON.code_user = req.body.code_user;
+  if (req.body.code_session) objJSON.code_session = req.body.code_session;
+  if (req.body.code_current) objJSON.code_current = req.body.code_current;
+  if (req.body.code_before) objJSON.code_before = req.body.code_before;
+  if (req.body.input) objJSON.input = req.body.input;
+  if (req.body.output) objJSON.output = req.body.output;
+
+  findData(objJSON, function (result) {
+    res.send(result);
+  });
+});
+
+const insertData = function (objJSON, callback) {
+  const collection = db.collection("chatbot");
+  collection.insertOne(objJSON, function (err, result) {
+    assert.equal(null, err);
+    callback(result);
+  });
+};
+
+const updateData = function (objJSON, callback) {
+  const collection = db.collection("chatbot");
+  const code_current = objJSON.code_current;
+  collection.updateOne(
+    { code_current: code_current },
+    { $set: objJSON },
+    function (err, result) {
+      assert.equal(null, err);
+      callback(result);
+    }
+  );
+};
+
+const deleteData = function (objJSON, callback) {
+  const collection = db.collection("chatbot");
+  const code_current = objJSON.code_current;
+  collection.deleteOne({ code_current: code_current }, function (err, result) {
+    assert.equal(null, err);
+    callback(result);
+  });
+};
+
+const findData = function (objJSON, callback) {
+  const collection = db.collection("chatbot");
+  collection.find(objJSON).toArray(function (err, result) {
+    assert.equal(null, err);
+    callback(result);
+  });
+};
