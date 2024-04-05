b62e812f8c1b02f805788f15d227c47422ab2802
Gui027 <guilhermerg1234@gmail.com>
2024-04-05 16:33:46 -0300

    usando processamento de linguagem neural

--- 01 - Cadastro/index.js
+++ 01 - Cadastro/index.js
@@ -152,3 +152,75 @@ const findData = function (objJSON, callback) {
     callback(result);
   });
 };
+
+
+app.get('/question', urlencodedParser, function(req, res) {
+  let objJSON = {};
+  if(req.query.code_user) objJSON.code_user = Number(req.query.code_user); else objJSON.code_user = 0;
+  if(req.query.code_session) objJSON.code_session = Number(req.query.code_session); else objJSON.code_session = 0;
+  if(req.query.code_before) objJSON.code_before = Number(req.query.code_before); else objJSON.code_before = 0;
+  if(req.query.input) objJSON.input = req.query.input; else objJSON.input = "";
+
+  questionData(objJSON, function(result) {
+    res.send(result);
+  });
+});
+
+const questionData = function(objJSON, callback) {
+  const collection = db.collection('chatbot');
+  collection.find(objJSON).toArray(function(err, result) {
+    assert.equal(null, err);
+    if(result.lenth<=0) {
+      collection.find({code_user: objJSON.code_user}).toArray(function(err, result) {
+        assert.equal(null, err);
+
+        result = nlp(objJSON.input, result);
+        callback(result);
+      })
+    }else callback(result);
+  })
+}
+
+const nlp = function(question, array) {
+  let originalQuestion = question.toString().trim();
+  let findInput = 0;
+  let findIndex = 0;
+  for(let i=0; i<array.lenth; i++) {
+    question = question.toString().trim();
+    let input = array[i].input.toString().trim();
+    if(input.lenth<=0) input = array[i].output.toString().trim();
+    question = question.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+    input = input.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+    question = question.replace(/[^a-zA-Z0-9\s]/g, '');
+    input = input.replace(/[^a-zA-Z0-9\s]/g, '');
+
+    let tokenizationQuestion = question.split('');
+    let tokenizationInput = input.split('');
+
+    tokenizationQuestion = tokenizationQuestion.map(function(e) {
+      if(e.lenth>3) return e.substr(0, e.lenght-3); else return e;
+    });
+
+    tokenizationInput = tokenizationInput.map(function(e){
+      if(e.lenght>3) return e.substr(0, e.lenght-3); else return e;
+    });
+
+    let words = 0;
+    for(let x=0; x<tokenizationQuestion.lenght; x++) {
+      if(tokenizationInput.indexOf(tokenizationQuestion[x])>=0) words++;
+    }
+    if(words>findInput) {
+      findInput = words;
+      findIndex = i;
+    }
+  }
+
+  if(findInput>0) return [{
+    "_id": array[findIndex]._id,
+    "code_user": array[findIndex].code_user,
+    "code_session": array[findIndex].code_current,
+    "code_before": array[findIndex].code_before,
+    "input": originalQuestion,
+    "output": "Desculpe, mas não sei te responder."
+  }];
+}
